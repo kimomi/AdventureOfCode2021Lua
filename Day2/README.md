5ff5d54e2397b6cf2230cbfe8bff224cbd95fe11
kimomi <dolamixie@163.com>
2021-12-09 10:11:09 +0800

    Day2

---
+++ Day2/README.md
@@ -0,0 +1,7 @@
+# Day 2
+## original question link
+https://adventofcode.com/2021/day/2
+## solution
+- parse string into num
+- do what you are told
+## diffcult  ★☆☆☆☆
---
+++ Day2/day2.lua
@@ -0,0 +1,46 @@
+local io = require 'io'
+
+function solution1()
+    local x = 0
+    local y = 0
+    for line in io.lines('data') do
+        for k, num in string.gmatch(line, "(%a+) (%d+)") do
+            if k == "down" then
+                y = y + tostring(num)
+            elseif k == "up" then
+                y = y - tostring(num)
+            elseif k == "forward" then
+                x = x + tostring(num)
+            else
+                error("unexpect type:" .. k)
+            end
+        end
+    end
+    
+    print(x * y)
+end
+
+function solution2()
+    local x = 0
+    local y = 0
+    local aim = 0
+    for line in io.lines('data') do
+        for k, num in string.gmatch(line, "(%a+) (%d+)") do
+            if k == "down" then
+                aim = aim + tostring(num)
+            elseif k == "up" then
+                aim = aim - tostring(num)
+            elseif k == "forward" then
+                local n = tostring(num)
+                x = x + n
+                y = y + aim * n
+            else
+                error("unexpect type:" .. k)
+            end
+        end
+    end
+
+    print(x * y)
+end
+
+solution2()
